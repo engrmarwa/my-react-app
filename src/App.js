7fa366f54609788dafe4abeb15c3de6a7e113b30
engrmarwa <engrmarwakhalid@gmail.com>
2025-05-15 17:09:23 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Name: Marwa Khalid</h1>
+      <h2>Registration Number: 21MDSWE168</h2>
+    </div>
+  );
+}
+
+export default App;
